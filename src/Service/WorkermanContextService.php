093f68311b798f158c3abef004b8815c0ab805e0
github-ci <you@example.com>
2025-04-18 02:40:52 +0000

    refactor(packages/workerman-server-bundle): 添加装饰器注入和上下文检查

--- src/Service/WorkermanContextService.php
+++ src/Service/WorkermanContextService.php
@@ -3,24 +3,42 @@
 namespace Tourze\WorkermanServerBundle\Service;
 
 use Symfony\Component\DependencyInjection\Attribute\AsDecorator;
+use Symfony\Component\DependencyInjection\Attribute\AutowireDecorated;
 use Tourze\Symfony\RuntimeContextBundle\Service\ContextServiceInterface;
 use Workerman\Coroutine;
+use Workerman\Worker;
 
 #[AsDecorator(decorates: ContextServiceInterface::class)]
 class WorkermanContextService implements ContextServiceInterface
 {
+    public function __construct(
+        #[AutowireDecorated] private readonly ContextServiceInterface $inner,
+    )
+    {
+    }
+
     public function getId(): string
     {
-        Coroutine::getCurrent()->id();
+        if (!Worker::isRunning()) {
+            return $this->inner->getId();
+        }
+        return Coroutine::getCurrent()->id();
     }
 
     public function defer(callable $callback): void
     {
+        if (!Worker::isRunning()) {
+            $this->inner->defer($callback);
+            return;
+        }
         Coroutine::defer($callback);
     }
 
     public function supportCoroutine(): bool
     {
+        if (!Worker::isRunning()) {
+            return $this->inner->supportCoroutine();
+        }
         return Coroutine::isCoroutine();
     }
 }
